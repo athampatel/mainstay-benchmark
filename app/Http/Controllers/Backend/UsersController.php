95f226923bb256d9f1725dbcbe9388edac8d5332
gokul <gokulnr@tendersoftware.in>
2023-01-18 12:45:14 +0530

    Added the email reset password after the activation

--- app/Http/Controllers/Backend/UsersController.php
+++ app/Http/Controllers/Backend/UsersController.php
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
 
 class UsersController extends Controller
 {
@@ -212,6 +214,32 @@ class UsersController extends Controller
             $user->active = 1;
             $user->activation_token = '';
             $user->save();
+            // email send work start
+
+            // one solution
+            
+            // $status = Password::sendResetLink(
+            //     [ 'email' =>'gokul12@yopmail.com']
+            // );
+            
+            // another solution
+            $token = Str::random(30);
+            $_token = Hash::make($token);
+
+            DB::table('password_resets')->insert(
+                // ['email' => $user->email, 'token' => $token, 'created_at' => date('Y-m-d h:i:s')]
+                ['email' => $user->email, 'token' => $_token, 'created_at' => date('Y-m-d h:i:s')]
+            );
+
+            $params = array('mail_view' => 'emails.user-active', 'subject' => 'reset password link', 'url' => env('APP_URL').'/reset-password/'.$token.'?email='.$user->email);
+            // $email = 'gokul12@yopmail.com';
+            // $params = array('mail_view' => 'emails.user-active', 'subject' => 'reset password link', 'url' => env('APP_URL').'/reset-password/'.$token.'?email='.$email);
+            // \Mail::to($user->email)->send(new \App\Mail\SendMail($params));
+            // http://localhost:8081/reset-password/b168354b23c967996fd7faadd74d6a07c59ae9eacd05f27e9f99b0b9789b0d12?email=
+            // http://localhost:8081/reset-password/3ea18ac218cd71f1f2973a6e828dc022e5f36455abffaf488607fcfac09005fc?email=gokul12@yopmail.com
+            \Mail::to('gokulnr@tendersoftware.in')->send(new \App\Mail\SendMail($params));
+
+            // email send work end
             $res = ['success' => true, 'message' =>'User activated successfully and email sent'];
         } else {
             $res = ['success' => false, 'message' =>'User not found'];
