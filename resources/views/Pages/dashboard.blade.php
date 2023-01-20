df863bac33c2a81af61d975045d057a4101cf65f
gokul <gokulnr@tendersoftware.in>
2023-01-20 17:23:49 +0530

    Added changes on the change order page

--- resources/views/Pages/dashboard.blade.php
+++ resources/views/Pages/dashboard.blade.php
@@ -146,71 +146,6 @@
                     </td>
                     <td>Completed</td>
 									</tr> --}}
-                  {{-- <tr>
-                  <td><a href="javascript:void(0)" class="item-number font-12 btn btn-primary btn-rounded">#89742</a></td>
-										<td><a href="javascript:void(0)" class="customer-name">Adams Baker</a></td>
-                    <td><a href="mailto:adamsbaker@mail.com" class="customer-name">adamsbaker@mail.com</a></td>
-										<td>2</td>
-										<td>$245</td>
-										<td>Apr 08, 2021</td>
-										<td><span class="svg-icon location-icon">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="8.542" height="11.46" viewBox="0 0 8.542 11.46"><path class="location-svg" d="M260.411,154a4.266,4.266,0,0,0-4.266,4.266c0,2.494,2.336,5.48,3.551,6.872a.952.952,0,0,0,1.428,0c1.217-1.385,3.563-4.37,3.563-6.872A4.266,4.266,0,0,0,260.411,154Zm0,6.7a2.439,2.439,0,1,1,1.724-.714A2.438,2.438,0,0,1,260.411,160.7Z" transform="translate(-256.145 -154)" fill="#9fcc47"/></svg>
-                        </span>London
-                    </td>
-                    <td>Completed</td>
-									</tr>
-                  <tr>
-                  <td><a href="javascript:void(0)" class="item-number font-12 btn btn-primary btn-rounded">#89742</a></td>
-										<td><a href="javascript:void(0)" class="customer-name">Adams Baker</a></td>
-                    <td><a href="mailto:adamsbaker@mail.com" class="customer-name">adamsbaker@mail.com</a></td>
-										<td>2</td>
-										<td>$245</td>
-										<td>Apr 08, 2021</td>
-										<td><span class="svg-icon location-icon">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="8.542" height="11.46" viewBox="0 0 8.542 11.46"><path class="location-svg" d="M260.411,154a4.266,4.266,0,0,0-4.266,4.266c0,2.494,2.336,5.48,3.551,6.872a.952.952,0,0,0,1.428,0c1.217-1.385,3.563-4.37,3.563-6.872A4.266,4.266,0,0,0,260.411,154Zm0,6.7a2.439,2.439,0,1,1,1.724-.714A2.438,2.438,0,0,1,260.411,160.7Z" transform="translate(-256.145 -154)" fill="#9fcc47"/></svg>
-                        </span>London
-                    </td>
-                    <td>Completed</td>
-									</tr>
-                  <tr>
-                  <td><a href="javascript:void(0)" class="item-number font-12 btn btn-primary btn-rounded">#89742</a></td>
-										<td><a href="javascript:void(0)" class="customer-name">Adams Baker</a></td>
-                    <td><a href="mailto:adamsbaker@mail.com" class="customer-name">adamsbaker@mail.com</a></td>
-										<td>2</td>
-										<td>$245</td>
-										<td>Apr 08, 2021</td>
-										<td><span class="svg-icon location-icon">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="8.542" height="11.46" viewBox="0 0 8.542 11.46"><path class="location-svg" d="M260.411,154a4.266,4.266,0,0,0-4.266,4.266c0,2.494,2.336,5.48,3.551,6.872a.952.952,0,0,0,1.428,0c1.217-1.385,3.563-4.37,3.563-6.872A4.266,4.266,0,0,0,260.411,154Zm0,6.7a2.439,2.439,0,1,1,1.724-.714A2.438,2.438,0,0,1,260.411,160.7Z" transform="translate(-256.145 -154)" fill="#9fcc47"/></svg>
-                        </span>London
-                    </td>
-                    <td>Completed</td>
-									</tr>
-                  <tr>
-                  <td><a href="javascript:void(0)" class="item-number font-12 btn btn-primary btn-rounded">#89742</a></td>
-										<td><a href="javascript:void(0)" class="customer-name">Adams Baker</a></td>
-                    <td><a href="mailto:adamsbaker@mail.com" class="customer-name">adamsbaker@mail.com</a></td>
-										<td>2</td>
-										<td>$245</td>
-										<td>Apr 08, 2021</td>
-										<td><span class="svg-icon location-icon">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="8.542" height="11.46" viewBox="0 0 8.542 11.46"><path class="location-svg" d="M260.411,154a4.266,4.266,0,0,0-4.266,4.266c0,2.494,2.336,5.48,3.551,6.872a.952.952,0,0,0,1.428,0c1.217-1.385,3.563-4.37,3.563-6.872A4.266,4.266,0,0,0,260.411,154Zm0,6.7a2.439,2.439,0,1,1,1.724-.714A2.438,2.438,0,0,1,260.411,160.7Z" transform="translate(-256.145 -154)" fill="#9fcc47"/></svg>
-                        </span>London
-                    </td>
-                    <td>Completed</td>
-									</tr>
-                  <tr>
-                  <td><a href="javascript:void(0)" class="item-number font-12 btn btn-primary btn-rounded">#89742</a></td>
-										<td><a href="javascript:void(0)" class="customer-name">Adams Baker</a></td>
-                    <td><a href="mailto:adamsbaker@mail.com" class="customer-name">adamsbaker@mail.com</a></td>
-										<td>2</td>
-										<td>$245</td>
-										<td>Apr 08, 2021</td>
-										<td><span class="svg-icon location-icon">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="8.542" height="11.46" viewBox="0 0 8.542 11.46"><path class="location-svg" d="M260.411,154a4.266,4.266,0,0,0-4.266,4.266c0,2.494,2.336,5.48,3.551,6.872a.952.952,0,0,0,1.428,0c1.217-1.385,3.563-4.37,3.563-6.872A4.266,4.266,0,0,0,260.411,154Zm0,6.7a2.439,2.439,0,1,1,1.724-.714A2.438,2.438,0,0,1,260.411,160.7Z" transform="translate(-256.145 -154)" fill="#9fcc47"/></svg>
-                        </span>London
-                    </td>
-                    <td>Completed</td>
-									</tr>					 --}}
 								</tbody>
 								<tfoot>
                 <tr>
@@ -237,7 +172,3 @@
     </div>
   </div>  <!--- home-content ---->
 @endsection
-
-<script type="text/javascript">
-  // const logged_customer= '{{Auth::user()}}';
-</script>
