082364a36142b69e82650f225dde0572247f5025
gokul <gokulnr@tendersoftware.in>
2023-01-30 18:38:46 +0530

    Added the logout menu changes

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -10,14 +10,22 @@ sidebarBtn.onclick = function() {
 
 $(document).on("click", "#logout", function(e){
     // e.preventDefault()
-    $.ajax({
-        type: 'POST',
-        url: '/logout',
-        dataType: "JSON",
-        data: { "_token": $('meta[name="csrf-token"]').attr('content')},
-        success: function (res) {
-            window.location.reload();
-            // window.location = 'http://www.google.com';
-        }
-    });
+    // console.log('__clicked');
+    // $.ajax({
+    //     type: 'POST',
+    //     url: '/logout',
+    //     dataType: "JSON",
+    //     data: { "_token": $('meta[name="csrf-token"]').attr('content')},
+    //     success: function (res) {
+    //         window.location.reload();
+    //         // window.location = 'http://www.google.com';
+    //     }
+    // });
+    // $('#logout-link').click(function(e) {
+        e.preventDefault();
+        $('<form action="/logout" method="post">' +
+          '<input type="hidden" name="_token" value="{{ csrf_token() }}">' +
+          '</form>').submit();
+          console.log('__submitted');
+    //   });
 });
