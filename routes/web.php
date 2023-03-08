2fb91026af34ae796c4f0db8b1643b387d1b15fd
Atham <atham@tendetsoftware.in>
2023-03-08 16:50:48 +0530

    route name changed

--- routes/web.php
+++ routes/web.php
@@ -112,8 +112,8 @@ Route::get('/products/fetch-sde-manual',[ProductsController::class,'getProducts'
 Route::get('/products/import-sde-manual',[ProductsController::class,'CheckUpdateProducts'])->name('import-products');
 
 
-Route::get('/invoiced-orders/fetch-sde-manual',[SchedulerLogController::class,'getProducts'])->name('fectch-products');
-Route::get('/invoiced-orders/invoice-sde-manual',[InvoicedOrdersController::class,'getInvoiceOrders'])->name('import-products');
+Route::get('/invoiced-orders/fetch-sde-manual',[SchedulerLogController::class,'getProducts'])->name('fectch-invoice');
+Route::get('/invoiced-orders/invoice-sde-manual',[InvoicedOrdersController::class,'getInvoiceOrders'])->name('import-invoice');
 
     //$invoice = new InvoicedOrdersController();
         //$invoice->getInvoiceOrders();
