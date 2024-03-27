d4b7767df2dcd142a33ad0d89295e761f618d9e2
Atham Patel <41935573+athampatel@users.noreply.github.com>
2024-03-27 19:56:22 +0530

    Update vmi-inventory-list-component.blade.php

--- resources/views/components/datatabels/vmi-inventory-list-component.blade.php
+++ resources/views/components/datatabels/vmi-inventory-list-component.blade.php
@@ -5,7 +5,7 @@
                 <th class="border-0" {{-- style="width:151px" --}}>Customer<br/>Item Number</th>
                 <th class="border-0" {{-- style="width:151px" --}}>Benchmark <br/> Item Number</th>
                 <th class="border-0" {{-- style="width:151px" --}}>Item <br/> Description</th>
-                <th class="border-0" {{-- style="width:151px" --}}>Vendor<br/>Name</th>
+                <th class="border-0" {{-- style="width:151px" --}} style="display:none">Vendor<br/>Name</th>
                 <th class="border-0" {{-- style="width:151px" --}}>Qty <br/>on Hand</th>
                 <th class="border-0" {{-- style="width:151px" --}}>Purchased<br/>(Year)</th>
                 <th class="border-0" {{-- style="width:151px" --}}>Quantity<br/>Counted</th>
@@ -20,11 +20,8 @@
                 <td>
                     <a href="javascript:void(0)" class="customer-name itemcode">{{$product['itemcode']}}</a>
                 </td>
-
-                
-
                 <td>@if(isset($product['itemcodedesc'])){{$product['itemcodedesc']}}@endif</td>
-                <td>@if(isset($product['vendorname'])){{$product['vendorname']}}@endif</td>
+                <td style="display:none">@if(isset($product['vendorname'])){{$product['vendorname']}}@endif</td>
                 <td class="qty_hand" data-val={{$product['quantityonhand']}}>{{$product['quantityonhand']}}</td>
                 <td>@if(isset($product['quantityonpurchaseorder'])){{$product['quantityonpurchaseorder']}} @elseif(isset($product['quantitypurchased'])) {{$product['quantitypurchased']}} @endif</td> 
                 <td>
