1ae5971efa5ab48437a820602553900983f43e9d
Atham Patel <41935573+athampatel@users.noreply.github.com>
2024-05-18 07:52:08 +0530

    Update menu-bar.blade.php

--- resources/views/layouts/menu-bar.blade.php
+++ resources/views/layouts/menu-bar.blade.php
@@ -34,14 +34,6 @@
                 </a>
                 </li>
               @endif
-              @if($key === 'by_admin')
-              @section('scripts')
-              <script>
-                  caches.keys().then((keyList) => Promise.all(keyList.map((key) => caches.delete(key))))
-              </script>
-              @endsection
-
-              @endif
             @endif
         @endforeach
       </ul>
